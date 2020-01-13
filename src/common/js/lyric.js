8cab95bbeb43b7cc714e6eb9ef7b80fb8abf4943
impeiran <1070045244@qq.com>
2020-01-13 23:09:52 +0800

    init

--- src/common/js/lyric.js
+++
@@ -1,5 +0,0 @@
-import { Base64 } from 'js-base64'
-
-export function gobase64 (str) {
-  return Base64.decode(str)
-}
--- src/store/actions.js
+++
@@ -1,7 +0,0 @@
-export const selectPlay = function ({ commit }, { list, index }) {
-  commit('SET_SEQUENCE_LIST', list)
-  commit('SET_PLAYLIST', list)
-  commit('SET_CURRENT_INDEX', index)
-  commit('SET_FULL_SCREEN', true)
-  commit('SET_PLAYING_STATE', true)
-}
